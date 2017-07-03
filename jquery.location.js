8bd262ddb0cd6f684da0fe38541f9889fa582a61
katayori <katayori323@gmail.com>
2017-07-03 14:54:44 +0900

    ページ遷移できるようになってるでー

--- jquery.location.js
+++ jquery.location.js
@@ -2,26 +2,26 @@
   $.fn.hoge=function(){
   $(this).click(function(){
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(get_location,no_get_location) ;
+      function get_location(argPos) {
         var lat;
         var log;
-
-      function get_location(argPos) {
         lat = argPos.coords.latitude;          // 緯度
         log = argPos.coords.longitude;         // 経度
         console.log(lat,log);
-
-
-
+        result_location(argPos.coords);
       }
+
       function no_get_location(error) {
         //.エラーコードの定義
       //エラーの表示
       alert("失敗１");
       }
+      navigator.geolocation.getCurrentPosition(get_location,no_get_location) ;
+
     }else {
       alert("失敗２")
     }
   });
   };
+
 })(jQuery);
