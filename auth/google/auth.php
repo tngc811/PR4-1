97c6135998bb70f6f7fa8e58843c576ff295f3b3
sakanaa114 <to_roku1213@yahoo.co.jp>
2017-08-14 13:02:44 +0900

    googleだけ認証できるようにしました。 ローカルで設定してる部分あるからこのままだと動かないと思います。

---
+++ auth/google/auth.php
@@ -0,0 +1,20 @@
+<?php
+// アプリケーション設定
+define('CONSUMER_KEY', '580811002788-3as05vpski0tqibi9v96skii4r4n6udn.apps.googleusercontent.com');
+//サーバにおいてるcallback.phpを指定する。
+//またgoogle側も設定を変更する必要がある。
+define('CALLBACK_URL', 'http://localhost/auth/google/callback.php');
+// URL
+define('AUTH_URL', 'https://accounts.google.com/o/oauth2/auth');
+//--------------------------------------
+// 認証ページにリダイレクト
+//--------------------------------------
+$params = array(
+	'client_id' => CONSUMER_KEY,
+	'redirect_uri' => CALLBACK_URL,
+	'scope' => 'openid profile email',
+	'response_type' => 'code',
+);
+
+// リダイレクト
+header("Location: " . AUTH_URL . '?' . http_build_query($params));
